3463c70fc0b6fb6c93d7a2edb5b39c891730504e
Erwan d'Orgeville <info@erwandorgeville.com>
2015-10-14 20:23:27 -0400

    Update to OMF

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,4 +1,11 @@
+# Path to Oh My Fish install.
+set -gx OMF_PATH /home/ct/.local/share/omf
 
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG /home/ct/.config/omf
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
 
 # Custom stuff
 set --erase fish_greeting
